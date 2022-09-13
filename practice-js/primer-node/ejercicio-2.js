37f1acede1e91081dcb199f1199a384a31db8759
Diego Senior <110189872+Dsenior21@users.noreply.github.com>
2022-09-13 16:48:23 -0500

    init commit

---
+++ practice-js/primer-node/ejercicio-2.js
@@ -0,0 +1,9 @@
+const lista = ["Diego", 24, true];
+
+const libro = {
+    titulo: "El quijote",
+    autor: "Cervantes",
+    fecha: 1615,
+    url: "http://www.daemcopiapo.cl/Biblioteca/Archivos/7_6253.pdf",
+
+}
