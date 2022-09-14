670279d0925068d8629d5eb8852dac7a4bfc0347
Diego Senior <110189872+Dsenior21@users.noreply.github.com>
2022-09-13 23:19:47 -0500

    init commit

---
+++ practice-js/primer-node/factorial-for.js
@@ -0,0 +1,10 @@
+let num = 10;
+let n = 1;
+for (let i = num; i>0; i--){
+    n *=i;
+}
+console.log(n)
+
+
+
+ 
---
+++ practice-js/primer-node/factorial-while.js
@@ -0,0 +1,11 @@
+let num = 10;
+let n = 1;
+let resultado = 1;
+
+while (n <= num) {
+  resultado *= num;
+  console.log(resultado);
+  num--;
+    
+    
+}
