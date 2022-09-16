1821013c5f1f7c7d5ad953143c30dce6681c43d4
Diego Senior <110189872+Dsenior21@users.noreply.github.com>
2022-09-16 15:51:09 -0500

    init commit

---
+++ practice-js/primer-node/ejercicio-4.js
@@ -0,0 +1,13 @@
+
+let altura = 185;
+let altura_2= 1.85;
+let peso = 76.9;
+
+let altuRed = Math.ceil(altura_2);
+console.log(altuRed)
+
+let pesoRed = Math.floor(peso);
+console.log(pesoRed)
+
+//let valueMax = 
+
